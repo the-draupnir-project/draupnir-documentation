17891c51a80fb3ebead16c5f07e23465ae8d67dd
gnuxie <Gnuxie@protonmail.com>
2024-06-14 16:21:28 +0100

    edit url was wrong.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/the-draupnir-project/draupnir-documentation/tree/main/website',
+            'https://github.com/the-draupnir-project/draupnir-documentation/tree/main/',
           routeBasePath: '/',
         },
         blog: false,
